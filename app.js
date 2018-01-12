2f72e921dfd733f28bcaa59d920bfad4698d9d71
MrGrandeFrite <gaetan13.vergnolle@orange.fr>
2018-01-12 14:24:25 +0100

    video done

--- app.js
+++ app.js
@@ -9,11 +9,85 @@ var sockets = [];
 const svrport = 3000;
 
 var counter = 0;
+var videoCounter = 1011;
+var imageCounter = 0;
 
 console.log('Node.js server is ready.');
 
 /* Connect to the database */
 db.this.connect(db.isConnected());
+
+
+
+
+/* SOCKET UDP SERVER */
+/* **************** */
+const PORT = 3001;
+//const HOST = "192.168.1.89";
+//const HOST = "192.168.1.34";
+const HOST = "192.168.43.250";
+//const HOST = "192.168.1.22";
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+var c = 0;
+var ack = "";
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    if(message.toString() === "EOF") {
+        db.this.query('INSERT INTO Videos(idVideo, urlImage, urlVideo, duration, date, title, idUser,idRobot) VALUES(' +
+            videoCounter + ',' +
+            '\'' + videoDirectory + 'thumbnail_' + imageCounter + '.png' + '\'' + ',' +
+            '\'' + videoDirectory + 'VIDEO_' + counter + '.mp4' + '\'' + ',' +
+            '\'23\'' + ',' +
+            '\'2017-11-12\'' + ',' +
+            '\'' + 'VIDEO_' + counter + '\'' + ',' +
+            '1' + ',' +
+            '1' +
+            ');')
+            .on('error', function (err) {
+                ack = new Buffer("VRR");
+                server.send(ack, 0, ack.length, remote.port, remote.address, function (err, bytes) {
+                    console.log("sent VRR");
+                });
+                console.log(err);
+            })
+            .on('result', function () {
+                ack = new Buffer("VWR");
+                server.send(ack, 0, ack.length, remote.port, remote.address, function (err, bytes) {
+                    console.log("sent VWR");
+                });
+                console.log('Robot insertion succeeded.');
+            });
+
+        videoCounter++;
+        imageCounter++;
+        counter++;
+        c = 0;
+
+    } else {
+        console.log(remote.address + ':' + remote.port + ' - ' + 'VIDEO_' + counter + 'packet' + c++);
+        fs.appendFile(videoDirectory + 'VIDEO_' + counter + '.mp4', message, function (err) {
+            if (err) throw err;
+        });
+        ack = new Buffer("R");
+        server.send(ack, 0, ack.length, remote.port, remote.address, function (err, bytes) {
+            console.log("sent R");
+        });
+    }
+
+});
+
+server.bind(PORT, HOST);
+
+
+
+
+
 
 var svr = net.createServer(function(sock) {
 
@@ -32,7 +106,7 @@ var svr = net.createServer(function(sock) {
     });
 
     sock.on('end', function() {
-        console.log('Dsiconnected :' + sock.remoteAddress + ':' + sock.remotePort);
+        console.log('Diconnected :' + sock.remoteAddress + ':' + sock.remotePort);
 
         if(sockets.indexOf(sock) !== -1) {
             delete sockets[sockets.indexOf(sock)];
@@ -44,9 +118,9 @@ var svr = net.createServer(function(sock) {
 svr.listen(svrport);
 
 var webClient = new net.Socket();
-webClient.connect(60372, function() {    
+webClient.connect(60372, function() {
     console.log('Spring server is ready.');
-    
+
 	webClient.on('data', function(data) {
 	    handleServerData(sock, data);
     });
@@ -58,7 +132,7 @@ webClient.connect(60372, function() {
 	webClient.on('end', function() {
 		console.log('Spring server disconnected.');
 	});
-    
+
 }).on('error', function () {
     console.log('Spring server not found.');
 });
@@ -91,7 +165,7 @@ var handleClientData = function(sock, data) {
         const robot  = {
 			numSerie : messageObj.SERIAL_NUMBER
 		};
-        
+
         insertRobot(sock, robot);
 		//checkUser(messageObj.LOGIN, messageObj.PASS);
         //const fakeUserId = 'user42';
@@ -102,13 +176,24 @@ var handleClientData = function(sock, data) {
 	    if (message === 'EOF') {
             counter = counter + 1;
             sock.write('RECEIVED\n');
-            //insertVideo(video);
-	    } else {
+            console.log("DC_RECEIVED");
+
+	    } else if (data.toString().endsWith("EOF")) {
+            fs.appendFile(videoDirectory + 'VIDEO_' + counter + '.mp4', data.toString().substring(0, data.toString().indexOf("onVideo")), function (err) {
+                if (err) throw err;
+            });
+            console.log("RECEIVED " + counter);
+            sock.write('DC_RECEIVED ' + counter++ + '\n');
+        } else {
             fs.appendFile(videoDirectory + 'VIDEO_' + counter + '.mp4', message, function (err) {
                 if (err) throw err;
             });
         }
-	} else {
+
+	} else if (event === 'TEST') {
+        sock.write("DC : YOUPI !!!\n");
+
+    } else {
 		fs.writeFile(videoDirectory + "error_" + counter + ".txt", event + "\n\n\n" + message, (err) => {
             if (err) {
                 console.log("FAILED");
@@ -129,9 +214,9 @@ getTag = function(data) {
 
     const stringData = data.toString();
 
-    if (stringData.includes("onVideo") ||
-		stringData.includes("onLogin") ||
-        stringData.includes("VALID")) {
+    if (stringData.includes("onLogin") ||
+        stringData.includes("VALID")  ||
+        stringData.startsWith("TEST")) {
         return stringData.substring(0, stringData.indexOf('/'));
     }
 
@@ -144,14 +229,14 @@ getMessage = function(data) {
     const stringData = data.toString();
 
     if (stringData.includes("onLogin") ||
-        stringData.includes("VALID")) {
+        stringData.includes("VALID")  ||
+        stringData.includes("TEST")) {
         return stringData.substring(stringData.indexOf('/') + 1);
     }
 
-	if (stringData.includes("onVideo")) {
-        const lenght = stringData.indexOf('/', stringData.indexOf('/') + 1);
-        return stringData.substring(lenght + 1);
-    }
+	/*if (stringData.includes("onVideo")) {
+        return stringData.substring(stringData.indexOf('/') + 1);
+    }*/
 
     return data;
 };
