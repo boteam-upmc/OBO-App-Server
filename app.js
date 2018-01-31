20b6d84c937ff904b4bcae4da0f96f358226d9f1
MrGrandeFrite <gaetan13.vergnolle@orange.fr>
2018-01-31 21:13:55 +0100

    maj

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const db = require('./database');
 const fs = require('fs');
 //const bcrypt = require('bcrypt')
 const androidClient = 'ANDROID/';
-const videoDirectory = process.cwd() + "/Video/";
+const videoDirectory = process.cwd() + "/resources/";
+const nameDirectory = "/resources/";
 
 var sockets = [];
 
 const svrport = 3000;
 
-var counter = 0;
-var videoCounter = 200;
-var imageCounter = 0;
+var counter = new Date().getMinutes() + "" + new Date().getSeconds() + "" + new Date().getUTCMilliseconds();
+//var videoCounter = 100;
+//var imageCounter = 100;
 
 console.log('Node.js server is ready.');
 
@@ -24,9 +25,9 @@ db.this.connect(db.isConnected());
 /* SOCKET UDP SERVER */
 /* **************** */
 const PORT = 3001;
-const HOST = "192.168.1.89";
+//const HOST = "192.168.1.89";
 //const HOST = "192.168.1.34";
-//const HOST = "192.168.43.250";
+const HOST = "192.168.43.250";
 //const HOST = "192.168.1.22";
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
@@ -40,25 +41,29 @@ server.on('listening', function () {
 });
 
 server.on('message', function (message, remote) {
-    if(message.toString() === "EOF") {
+    if(message.toString().startsWith("EOF")) {
+
+        var split = message.toString().split("/");
+
+        console.log(message.toString());
 
         dt = new Date();
 
-        db.this.query('INSERT INTO video(video_id, duration, image_url, robot_id, title, user_id, video_url, creation_date) VALUES(' +
-            videoCounter + ',' +
-            '\'23\'' + ',' +
-            '\'' + videoDirectory + 'thumbnail_' + imageCounter + '.png' + '\'' + ',' +
-            '1' + ',' +
-            '\'' + 'VIDEO_' + counter + '\'' + ',' +
-            '1' + ',' +
-            '\'' + videoDirectory + 'VIDEO_' + counter + '.mp4' + '\'' + ',' +
+        db.this.query('INSERT INTO video(video_id, creation_date, duration, image_url, robot_id, title, user_id, video_url) VALUES(' +
+            counter + ',' +
             '\'' + dt.getFullYear()
-                + '-' + dt.getMonth()+1
-                + '-' + dt.getDate()
-                + ' ' + dt.getHours()
-                + ':' + dt.getMinutes()
-                + ':' + dt.getSeconds()
-                + '.' + dt.getMilliseconds() + '\'' +
+            + '-' + dt.getMonth()+1
+            + '-' + dt.getDate()
+            + ' ' + dt.getHours()
+            + ':' + dt.getMinutes()
+            + ':' + dt.getSeconds()
+            + '.' + dt.getMilliseconds() + '\'' + ',' +
+            '\'23\'' + ',' +
+            '\'' + nameDirectory + 'thumbnail_' + counter + '.png' + '\'' + ',' +
+            split[2] + ',' +
+            '\'' + 'VIDEO_' + counter + '\'' + ',' +
+            split[1] + ',' +
+            '\'' + nameDirectory + 'VIDEO_' + counter + '.mp4' + '\'' +
             ');')
             .on('error', function (err) {
                 ack = new Buffer("VRR");
@@ -69,7 +74,7 @@ server.on('message', function (message, remote) {
             })
             .on('result', function () {
 
-                db.this.query('UPDATE user SET number_of_videos_taken = number_of_videos_taken + 1 WHERE user_id = 1;')
+                db.this.query('UPDATE user SET number_of_videos_taken = number_of_videos_taken + 1 WHERE user_id = ' + split[1] + ';')
                     .on('error', function (err) {
                         ack = new Buffer("VRR");
                         server.send(ack, 0, ack.length, remote.port, remote.address, function (err, bytes) {
@@ -86,9 +91,9 @@ server.on('message', function (message, remote) {
                     });
             });
 
-        videoCounter++;
-        imageCounter++;
-        counter++;
+        //videoCounter++;
+        //imageCounter++;
+        counter = new Date().getMinutes() + "" + new Date().getSeconds() + "" + new Date().getUTCMilliseconds();
         c = 0;
 
     } else {
@@ -174,16 +179,19 @@ var handleServerData = function(sock, data) {
 };
 
 var handleClientData = function(sock, data) {
-	if (data.length < 100 ) console.log('Received data from android: ' + data);
+	if (data.length < 200 ) console.log('Received data from android: ' + data);
 	else console.log('Received data from android');
 
 	const event = getTag(data);
 	const message = getMessage(data);
 
+    const messageObj = JSON.parse(message);
+
 	if (event === 'onLogin') {
 
-		const messageObj = JSON.parse(message);
+
         console.log("LOGIN  "+messageObj.LOGIN);
+        console.log("MDP " + messageObj.PASS);
 
         const robot  = {
 			numSerie : messageObj.SERIAL_NUMBER
@@ -195,10 +203,75 @@ var handleClientData = function(sock, data) {
         //webClient.write('ASSOC/' + fakeUserId + '/' + messageObj.SERIAL_NUMBER + '\r');
         webClient.write('ASSOC/' + 1 + '/' + 1 + '\r');
 
-	} else if (event === 'onVideo') {
+	}else if(event === 'onAssociation') {
+        //const messageObjAssoc = JSON.parse(message);
+        console.log("LOGIN  "+messageObj.LOGIN);
+        console.log("MDP " + messageObj.PASS);
+
+        const robotAssoc  = {
+            numSerie : messageObj.SERIAL_NUMBER
+        };
+
+        db.this.query('INSERT IGNORE INTO robot (serial_number) VALUES(' +
+            '\'' + robotAssoc.numSerie + '\'' +
+            ');')
+            .on('error', function (err) {
+                console.log(err);
+            })
+            .on('result', function () {
+                var request = 'SELECT robot_id FROM robot WHERE serial_number = ' +
+                    '\'' + robotAssoc.numSerie + '\'' +
+                    ';'
+                db.this.query(request, function (error, result, field) {
+                    if(error === null) {
+                        var id_robot = result[0].robot_id;
+
+                        var request = 'SELECT user_id FROM user WHERE password = ' +
+                            '\'' + messageObj.PASS + '\'' +
+                            ' AND username = ' +
+                            '\'' + messageObj.LOGIN + '\'' +
+                            ';'
+                        db.this.query(request, function (error, result, field) {
+                            if (error === null) {
+                                var id_user = result[0].user_id;
+                                console.log("GET ID ROBOT : " + id_robot);
+                                console.log("GET ID USER: " + id_user);
+
+
+                                db.this.query('INSERT INTO user_robot (user_id, robot_id, associated) VALUES(' +
+                                    id_user + ', ' +
+                                    id_robot + ', ' +
+                                    0 +
+                                    ');')
+                                    .on('error', function (err) {
+                                        console.log(err);
+                                    })
+                                    .on('result', function () {
+                                        sock.write("DATA/" + id_user + "/" + id_robot + '/\n');
+                                        console.log("Insert OK");
+                                    });
+
+                            } else {
+                                console.log(error);
+                            }
+                        });
+                    } else {
+                        console.log(error);
+                    }
+
+                });
+            });
+
+        /*var id_user = get_user_id(messageObj.LOGIN, messageObj.PASS);
+
+        console.log(id_user);
+
+        insert_association(id_user, id_robot);*/
+
+    }else if (event === 'onVideo') {
 	    if (message === 'EOF') {
             counter = counter + 1;
-            sock.write('RECEIVED\n');
+            //soReceived data from androidck.write('RECEIVED\n');
             console.log("DC_RECEIVED");
 
 	    } else if (data.toString().endsWith("EOF")) {
@@ -238,6 +311,7 @@ getTag = function(data) {
     const stringData = data.toString();
 
     if (stringData.includes("onLogin") ||
+        stringData.includes("onAssociation") ||
         stringData.includes("VALID")  ||
         stringData.startsWith("TEST")) {
         return stringData.substring(0, stringData.indexOf('/'));
@@ -252,6 +326,7 @@ getMessage = function(data) {
     const stringData = data.toString();
 
     if (stringData.includes("onLogin") ||
+        stringData.includes("onAssociation") ||
         stringData.includes("VALID")  ||
         stringData.includes("TEST")) {
         return stringData.substring(stringData.indexOf('/') + 1);
@@ -290,19 +365,18 @@ var checkUser = function(sock, user) {
         sock.write('Notidentified\n');
         console.log("NOT EGEAU");
      }
-else{
-   // const bddhashpass=resul[0].password;
-   // bcrypt.compare(user.PASS, bddhashpass, function(err, res) {
-    //  if(res){
-        console.log("OK EGEAU "+resul[0].passe);
-        sock.write('identified\n');
-    //  }else{
-        sock.write('Notidentified\n');
-        console.log("in else NOT EGEAU");
+    else{
+       const bddhashpass=resul[0].password;
+        if(user.PASS==bddhashpass) {
+            console.log("OK EGEAU " + resul[0].passe);
+            sock.write('identified\n');
+        } else {
+            sock.write('Notidentified\n');
+            console.log("in else NOT EGEAU");
 
-    //  }
-      // });
-}
+        }
+
+    }
 });
     
    /* db.this.query('INSERT IGNORE INTO robot SET ?', robot)
